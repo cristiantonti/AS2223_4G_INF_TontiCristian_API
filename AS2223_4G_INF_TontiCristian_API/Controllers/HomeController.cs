d03adc5e1691cb9584416d614d3692eed6f1982d
PC10 <PC10@INFO5-5381>
2023-03-16 08:13:33 +0100

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_TontiCristian_API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_TontiCristian_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
