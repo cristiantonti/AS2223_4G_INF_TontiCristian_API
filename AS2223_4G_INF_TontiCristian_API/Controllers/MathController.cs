9287712dd3b24d50b2d18aec47b92e5cf31ad296
PC10 <PC10@INFO5-5381>
2023-03-16 08:31:31 +0100

    save 2

--- AS2223_4G_INF_TontiCristian_API/Controllers/MathController.cs
+++ AS2223_4G_INF_TontiCristian_API/Controllers/MathController.cs
@@ -2,11 +2,26 @@
 
 namespace AS2223_4G_INF_TontiCristian_API.Controllers
 {
-    public class MathController : Controller
+    [ApiController]
+    [Produces("application/json")]
+    [Route("[controller]")]
+    public class HomeController : Controller
     {
+        [HttpGet("GetMath")]
         public IActionResult Index()
         {
             return View();
+        }
+        public string CheckEvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Il numero è pari";
+            }
+            else
+            {
+                return "Il numero è dispari";
+            }
         }
     }
 }
